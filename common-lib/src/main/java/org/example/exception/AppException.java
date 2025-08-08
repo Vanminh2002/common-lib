c19959fff4651e1537a536d72b6d8f8825a42d6d
Vanminh2002 <vanminh200220002@Gmail.com>
2025-08-08 16:04:55 +0700

    update exception handle

---
+++ common-lib/src/main/java/org/example/exception/AppException.java
@@ -0,0 +1,15 @@
+package org.example.exception;
+
+import lombok.Data;
+
+
+@Data
+public class AppException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
